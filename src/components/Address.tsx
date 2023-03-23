3fd9426ed82feda67ba2ded008ec8551b5532da9
punch <117326984+onetxpunch@users.noreply.github.com>
2023-03-22 22:03:46 -0400

    wah

--- src/components/Address.tsx
+++ src/components/Address.tsx
@@ -1,5 +1,23 @@
+import { useEffect, useState } from "react";
 const List = () => {
-  return <div>Hello</div>;
+  const [input, setInput] = useState("");
+  return (
+    <div className="flex flex-col items-center gap-8 justify-center m-4">
+      <div className="underline bolder text-6xl text-white">
+        Arbitrum Airdrop Tool 🪂
+      </div>
+      <div className="underline bolder text-4xl text-white">
+        Check Addresses:
+      </div>
+      <textarea
+        value={input}
+        onChange={(res) => {
+          console.log(res);
+          setInput(res.target.value);
+        }}
+      />
+    </div>
+  );
 };
 
 export default List;
